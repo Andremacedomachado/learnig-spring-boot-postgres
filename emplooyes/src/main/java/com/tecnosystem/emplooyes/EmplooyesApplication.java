c47b53865168bfc643fa15e79ee1ce8bfd0c643a
André Macedo Machado <andremacedomachado@gmail.com>
2021-07-25 08:39:55 -0300

    sincronização de arquivos iniciais

---
+++ emplooyes/src/main/java/com/tecnosystem/emplooyes/EmplooyesApplication.java
@@ -0,0 +1,13 @@
+package com.tecnosystem.emplooyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplooyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplooyesApplication.class, args);
+	}
+
+}
---
+++ emplooyes/src/test/java/com/tecnosystem/emplooyes/EmplooyesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnosystem.emplooyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplooyesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
